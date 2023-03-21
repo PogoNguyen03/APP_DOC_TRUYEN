8f62be483ec8068c260f783db1e0a2918d543f35
NguyenCanhPhong <Nguyencanhphong246@gmail.com>
2023-03-21 16:23:12 +0700

    chap list

---
+++ app/src/main/java/com/example/app_truyen_tranh/ChapActivity.java
@@ -0,0 +1,28 @@
+/*package com.example.app_truyen_tranh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ChapActivity extends AppCompatActivity {
+TextView txvTenTruyens;
+ImageView imgAnhTruyens;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chap);
+        init();
+        anhXa();
+        setUp();
+        setClick();
+    }
+    private void init(){}
+    private void anhXa(){
+        imgAnhTruyens = findViewById(R.id.imgAnhTruyen);
+        txvTenTruyens = findViewById(R.id.txvTenTruyens);
+    }
+    private void setUp(){}
+    private void setClick(){}
+}*/
